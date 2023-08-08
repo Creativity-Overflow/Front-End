b54802b85e1dfed4cf6c5be3ad2a97c7d02ae765
TheTeaser <theteaser98@gmail.com>
2023-08-08 13:27:48 +0000

    added SignUp

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     
 <header class="w-full text-gray-700 bg-blue-400 shadow-sm body-font">
     <div class="container flex flex-col items-start p-6 mx-auto md:flex-row">
-    <a
+    <Link
         class="mx-2 my-1 flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 lg:mb-0 lg:mt-0"
         href="/">
         <img
@@ -14,17 +14,17 @@ export default function Header() {
           style={{"height": "30px"}}
           alt="TE Logo"
           loading="lazy" />
-      </a>
+      </Link>
         <nav class="flex items-center justify-center text-base md:ml-auto">
-            <a href="/" class="mr-5 font-medium hover:text-gray-900">Home</a>
-            <a href="/About" class="mr-5 font-medium hover:text-gray-900">About Us</a>
+            <Link href="/" class="mr-5 font-medium hover:text-gray-900">Home</Link>
+            <Link href="/About" class="mr-5 font-medium hover:text-gray-900">About Us</Link>
         </nav>
         <div class="items-center h-full pl-6 ml-6 border-l border-gray-200">
-            <a href="/login" class="mr-5 font-medium hover:text-gray-950">Login</a>
-            <a href="#"
+            <Link href="/login" class="mr-5 font-medium hover:text-gray-950">Login</Link>
+            <Link href="/signup"
                 class="px-4 py-2 text-xs font-bold text-white uppercase transition-all duration-150 bg-gray-800 rounded shadow outline-none active:bg-teal-600 hover:shadow-md focus:outline-none ease">
                 Sign Up
-            </a>
+            </Link>
         </div>
     </div>
 </header>
---
+++ pages/signupartist.js
@@ -0,0 +1,13 @@
+import Parents from "@/components/Parents";
+import Image from "next/image";
+import SignUpArtist from "../components/SignUpArtist";
+
+export default function signup() {
+  return (
+    <>
+      <Parents>
+       <SignUpArtist />
+      </Parents>
+    </>
+  );
+}
