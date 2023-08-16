cf10e7001ac7b09762260ef8cebf451095e3fb47
SalahHammash <s.m.hammash@gmail.com>
2023-08-16 21:04:50 +0300

    changes on the artist details (username& button)

--- components/Artist_detail.js
+++ components/Artist_detail.js
@@ -10,17 +10,21 @@ export default function ArtistProfile ()  {
     return (
       <>
       <div className="flex justify-center items-center h-[screen]">
-      <div className="p-4 shadow-md rounded-3xl border border-[black] w-[80%] flex justify-center items-center h-[80%] mt-16" style={{ background: "rgba(0,0,0,.7)" }}>
+      <div className="p-4 shadow-md rounded-3xl border border-[black] w-[80%] flex justify-center items-center h-[80%] mt-11" style={{ background: "rgba(0,0,0,.7)" }}>
         <div className="flex items-center justify-around text-5xl"> 
           <img
             src={image}
             alt="Profile"
-            className="w-20 h-20 mr-72 "
-            style={{ width: '400px', height: '500px' }} 
+            className="w-52 h-30 mr-48 "
+            style={{ width: '450px', height: '550px' }} 
           />
-          <div>
-            <p className="text-gray-600">{username}</p>
-            <p className="text-gray-600">{email}</p>
+          <div className="mr-36">
+            <p className="text-white mr-52 mt-11">{username}</p>
+            <br>
+            </br>
+            <br>
+            </br>
+            <p className="text-white">{email}</p>
             <ButtonArt />
           </div>
           </div>
